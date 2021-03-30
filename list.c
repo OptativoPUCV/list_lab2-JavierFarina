d9ba17528ee2ea45b1f12127f05e93ffe7730955
Replit user <replituser@example.com>
2021-03-30 11:43:00 +0000

    new try

--- list.c
+++ list.c
@@ -115,13 +115,13 @@ void * popBack(List * list) {
 void * popCurrent(List * list) {
 if ( list->head == NULL || list->current->next == NULL)
   return NULL;
-Node* aux = list->current;
+const void* aux = list->current->data;
 if ( list->current == list->head ) {
   list->current->next->prev = NULL;
   list->head = list->head->next;
   free(list->current);
   list->current = list->head;
-  return (void*) aux->data;
+  return (void*)aux;
 }
 /*
 else {
